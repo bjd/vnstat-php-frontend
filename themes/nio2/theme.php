5217686b2bceb32947155eb824b64e584c1aa46b
Tunghsiao Liu <info@sparanoid.com>
2011-09-07 12:10:37 +0800

    ADD theme

---
+++ themes/nio2/theme.php
@@ -0,0 +1,15 @@
+<?php
+    $colorscheme = array(
+         'image_background'   => array( 255, 255, 255,   0 ),
+	 'graph_background'   => array( 242, 244, 248,   0 ),
+	 'graph_background_2' => array( 210, 215, 229,   0 ),
+	 'grid_stipple_1'     => array( 177, 187, 211,   0 ),
+         'grid_stipple_2'     => array( 242, 244, 248,   0 ),
+	 'border'             => array( 255, 255, 255,   0 ),
+	 'text'               => array(   0,   0,   0,   0 ),
+	 'rx'                 => array( 254, 186, 175,  50 ),
+	 'rx_border'	      => array(  136, 58, 45,  90 ),
+	 'tx'	              => array( 203, 243, 167,  50 ),
+	 'tx_border'          => array(  80, 124, 41,  90 )
+     );
+?>
