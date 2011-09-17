14f471653cc3e6f820c3a9f091eee7806807c9c9
Björge Dijkstra <bjd@jooz.net>
2011-09-17 16:46:29 +0200

    Update version

--- index.php
+++ index.php
@@ -188,7 +188,7 @@
     }
     ?>
     </div>
-    <div id="footer"><a href="http://www.sqweek.com/">vnStat PHP frontend</a> 1.5.1 - &copy;2006-2010 Bjorge Dijkstra (bjd _at_ jooz.net)</div>
+    <div id="footer"><a href="http://www.sqweek.com/">vnStat PHP frontend</a> 1.5.2 - &copy;2006-2011 Bjorge Dijkstra (bjd _at_ jooz.net)</div>
   </div>
 </div>
 
