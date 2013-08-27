3e8f2baa88f61b5c717118243464db31189c365b
Tunghsiao Liu <t@sparanoid.com>
2013-08-27 12:23:16 +0800

    Update Chinese Simplified language, adds Chinese Traditional

---
+++ lang/zh_tw.php
@@ -0,0 +1,39 @@
+<?php
+
+// sidebar labels
+$L['summary'] = '總覽';
+$L['hours'] = '按小時';
+$L['days'] = '按天';
+$L['months'] = '按月';
+
+// main table headers
+$L['Summary'] = '總覽';
+$L['Top 10 days'] = '最高 10 天流量排行';
+$L['Last 24 hours'] = '最近 24 小時';
+$L['Last 30 days'] = '最近 30 天天';
+$L['Last 12 months'] = '最近 12 個月';
+
+// traffic table columns
+$L['In'] = '流入';
+$L['Out'] = '流出';
+$L['Total'] = '合計';
+
+// summary rows
+$L['This hour'] = '最近一小時';
+$L['This day'] = '當天';
+$L['This month'] = '當月';
+$L['All time'] = '總計';
+
+// graph text
+$L['Traffic data for'] = 'Traffic data for ';
+$L['bytes in'] = 'bytes in';
+$L['bytes out'] = 'bytes out';
+
+// date formats
+$L['datefmt_days'] = '%m 月 %d 日';
+$L['datefmt_days_img'] = '%d';
+$L['datefmt_months'] = '%Y 年 %m 月';
+$L['datefmt_months_img'] = '%b';
+$L['datefmt_hours'] = '%l%P';
+$L['datefmt_hours_img'] = '%l';
+$L['datefmt_top'] = '%Y-%m-%d';
